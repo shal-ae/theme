585f39782e3a7546a52fcb2f434b7d2e1357005f
Alex Shubin <Alex.Shubin@gmail.com>
2021-06-20 16:38:16 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import { SquareComponent } from './square/square.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {MatGridListModule} from "@angular/material/grid-list";
+import { ShowThemeComponent } from './show-theme/show-theme.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SquareComponent,
+    ShowThemeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/show-theme/show-theme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-theme',
+  templateUrl: './show-theme.component.html',
+  styleUrls: ['./show-theme.component.scss']
+})
+export class ShowThemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
